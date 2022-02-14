b632517f01b874f278a2e1d656b45ca5e6cf5249
Dihfahsih1 <dihfahsihm@gmail.com>
2022-02-14 23:15:49 +0300

    learning abstractions

---
+++ yr-sem1/sicp/square.scm
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname square) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+
+
+(define p 3.14)
+(define radius 10)
+
+(define(area r)
+  (* p r r)
+  )
+
+(area radius)
