e0498592106fa051d9ac52ec6ddec067180c287d
Dihfahsih1 <dihfahsihm@gmail.com>
2022-03-17 12:50:49 +0300

    fix bugs in the code

--- yr-sem1/Network Programming/assignments/mail-client/mailClient.py
+++ yr-sem1/Network Programming/assignments/mail-client/mailClient.py
@@ -7,20 +7,24 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 
-server = smtplib.SMTP('smtp.gmail.com', 25)
 
-server.ehlo()
+context = ssl.create_default_context()
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465,context=context)
 
+sender_email ="dihfahsih@mail.com"
 with open('password.txt', 'r') as psw:
   password=psw.read()
-sender_email ="dihfahsih@mail.com"
+  
+server.ehlo()
 server.login(sender_email, password)
 
 msg = MIMEMultipart()
 msg['from']= "Mail Client"
+
 with open('mail.html', 'r', encoding="utf8") as file:
     data = file.read().replace('\n', '')
 count = 0
+
 with open("mail_list.csv") as file:
     reader = csv.reader(file)
     next(reader)
@@ -34,13 +38,6 @@ with open("mail_list.csv") as file:
 
         count += 1
         print(str(count) + ". Sent to " + email)
-
-        if(count%80 == 0):
-            server.quit()
-            print("Server cooldown for 100 seconds")
-            time.sleep(100)
-            server.ehlo()
-            server.login(sender_email, password)
 
 server.quit()
 
