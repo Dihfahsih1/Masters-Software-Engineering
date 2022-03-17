1c13803c0458aade853dac5024f3f470f65b2204
Dihfahsih1 <dihfahsihm@gmail.com>
2022-03-17 13:51:10 +0300

    fixed: the typo in the sending email was the cause

--- yr-sem1/Network Programming/assignments/mail-client/mailClient.py
+++ yr-sem1/Network Programming/assignments/mail-client/mailClient.py
@@ -6,17 +6,15 @@ from email import encoders
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
-
+import getpass
 
 context = ssl.create_default_context()
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465,context=context)
+port = 465
+smtp_server="smtp.gmail.com"
 
-sender_email ="dihfahsih@mail.com"
-with open('password.txt', 'r') as psw:
-  password=psw.read()
+sender_email ="dihfahsih@gmail.com"
+password = getpass.getpass("Type your password and press enter: ")
   
-server.ehlo()
-server.login(sender_email, password)
 
 msg = MIMEMultipart()
 msg['from']= "Mail Client"
@@ -25,19 +23,21 @@ with open('mail.html', 'r', encoding="utf8") as file:
     data = file.read().replace('\n', '')
 count = 0
 
-with open("mail_list.csv") as file:
-    reader = csv.reader(file)
-    next(reader)
-    for name, email in reader:
-        html = data.format(name=name)
-        msg.attach(MIMEText(html, "html"))
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+  server.login(sender_email, password)
+  
+  
+  with open("mail_list.csv") as file:
+      reader = csv.reader(file)
+      next(reader)
+      for name, email in reader:
+          html = data.format(name=name)
+          msg.attach(MIMEText(html, "html"))
 
-        server.sendmail(
-            sender_email, email, msg.as_string()
-        )
+          server.sendmail(sender_email, email, msg.as_string())
 
-        count += 1
-        print(str(count) + ". Sent to " + email)
+          count += 1
+          print(str(count) + ". Sent to " + email)
 
-server.quit()
+  server.quit()
 
