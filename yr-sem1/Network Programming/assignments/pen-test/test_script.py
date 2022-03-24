cf7be732ef2f90d71a6baec05eee48a4a8779253
Dihfahsih1 <dihfahsihm@gmail.com>
2022-03-24 08:51:40 +0300

    FIXED:fix the bug that was failing the script from executing

--- yr-sem1/Network Programming/assignments/pen-test/test_script.py
+++ yr-sem1/Network Programming/assignments/pen-test/test_script.py
@@ -5,10 +5,13 @@ import subprocess as sub
 interface = "wlp2s0"
 
 #fe80::2fd3:bfe6:6d8f:3efb
-new_mac = "08:00:27:02:3a:52"
+new_mac = "1C:4D:70:C8:6B:B8"
 
 sub.call(['sudo', 'ifconfig', interface, 'down'])
 
-sub.call(['sudo', 'ifconfig', interface, 'hw', 'wlp2s0', new_mac])
+sub.call(['sudo', 'ifconfig', interface, 'hw', 'ether', new_mac])
 
 sub.call(['sudo', 'ifconfig', interface, 'up'])
+
+#run ifconfig wlp2s0 before and after running this python script
+
