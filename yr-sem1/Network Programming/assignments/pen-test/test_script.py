b4ad58b9b442c657708c6562adfab1e61d3e42fb
Dihfahsih1 <dihfahsihm@gmail.com>
2022-03-24 10:00:28 +0300

    add alternatives to call()

--- yr-sem1/Network Programming/assignments/pen-test/test_script.py
+++ yr-sem1/Network Programming/assignments/pen-test/test_script.py
@@ -1,12 +1,14 @@
 #MAC address spoofing using Python:
 
 import subprocess as sub
+#subprocess => module used to run new codes and applications by creating new processes
 
-interface = "wlp2s0"
+interface = "wlp2s0" #wlp2s0 => wide local area network PCI bus 2 on slot zero
 
-#fe80::2fd3:bfe6:6d8f:3efb
 new_mac = "1C:4D:70:C8:6B:B8"
 
+#call() is used to initiate a program
+#alternatively you can use Popen and run()
 sub.call(['sudo', 'ifconfig', interface, 'down'])
 
 sub.call(['sudo', 'ifconfig', interface, 'hw', 'ether', new_mac])
